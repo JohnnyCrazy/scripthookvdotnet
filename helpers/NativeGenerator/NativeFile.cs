42c4287aad76a565f3bd9f73b15630c788b51b49
Johnny Dellinger @PC <johnny@johnnycrazy.de>
2015-05-31 21:36:09 +0200

    First NativeGenerator-Implementation

---
+++ helpers/NativeGenerator/NativeFile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace NativeGenerator
+{
+    public class NativeFile : Dictionary<string, NativeNamespace>
+    {
+    }
+
+    public class NativeNamespace : Dictionary<string, NativeFunction>
+    {
+    }
+
+    public class NativeFunction
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("params")]
+        public List<NativeParams> Params { get; set; }
+
+        [JsonProperty("results")]
+        public string Results { get; set; }
+
+        [JsonProperty("jhash")]
+        public string JHash { get; set; }
+    }
+
+    public class NativeParams
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
